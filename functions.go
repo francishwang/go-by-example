c07f3e54f90bb2a9ff491f9c77d271431e039bc8
Hyunseok Hwang <fr.hwang@gmail.com>
2013-05-01 20:03:09 +0900

    Add examples: range & functions

---
+++ functions.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func plus(a int, b int) int {
+  return a + b // Go requires explicit returns
+}
+
+func main() {
+  res := plus(1, 2)
+  fmt.Println("1+2 =", res)
+}
